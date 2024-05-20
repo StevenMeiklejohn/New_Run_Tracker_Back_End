25ba0596cadfd9348d3b061b51763afb12bc2a59
steven Meiklejohn <stevenmeiklejohn700@gmail.com>
2024-05-20 21:21:23 +0100

    Initial commit

---
+++ db/seeds.js
@@ -0,0 +1,15 @@
+use run_tracker;
+db.dropDatabase();
+
+db.runs.insertMany([
+    {
+        date: "20/5/24",
+        distance: 5,
+        time: 0.5
+    },
+    {
+        date: "19/5/24",
+        distance: 5,
+        time: 0.5
+    },
+]);
---
+++ server.js
@@ -0,0 +1,24 @@
+
+const express = require('express');
+const createRouter = require('./helpers/create_router');
+const app = express();
+const cors = require('cors')
+
+const MongoClient = require('mongodb').MongoClient
+
+app.use(express.json());
+app.use(cors());
+
+MongoClient.connect('mongodb://127.0.0.1:27017', {useUnifiedTopology: true})
+.then((client) => {
+    const db = client.db('run_tracker');
+    const runCollection = db.collection('runs')
+    const runsRouter = createRouter(runCollection);
+    app.use('/api/runs', runsRouter);
+})
+.catch(console.error)
+
+
+app.listen(9000, function (){
+    console.log("Listening on port 9000");
+})
